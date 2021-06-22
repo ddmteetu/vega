d9afd34b018a00068667a9a88596c79d04ca2453
Vishvendra Singh <ddmteetu@gmail.com>
2021-06-22 10:59:25 +0530

    populating drop-down lists

--- ClientApp/src/app/services/make.service.ts
+++ ClientApp/src/app/services/make.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MakeService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getMakes() {
     return this.http.get('/api/makes')
-      .map(res => res.json());
+      .pipe(map(res => res));
   }
 }
